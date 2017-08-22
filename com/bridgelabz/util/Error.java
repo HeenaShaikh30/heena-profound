389269481de3d0f56b05f431773cb3c851413845
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-22 18:03:38 +0530

    Add files via upload

---
+++ com/bridgelabz/util/Error.java
@@ -0,0 +1,16 @@
+/**************************************************************
+*Compilation: javac -d bin com/bridgelabz/util/Error.java
+*Execution:   java -cp bin com.bridgelabz.util.Error 
+*purpose:   Program for generating an error.
+*@author:  Heena Shaikh
+*@version:1.0
+*@since:10-8-2017
+***************************************************************/ 
+ package com.bridgelabz.util;
+ 
+ public class Error {
+	
+	public static void error(int x,int y){
+		System.out.println("**     *** *******    **                *               **");
+	}
+ }
