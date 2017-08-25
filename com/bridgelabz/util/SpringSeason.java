faf4e14a9cdab34a854f72398958a86ed432452b
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:49:54 +0530

    Update SpringSeason.java

--- com/bridgelabz/util/SpringSeason.java
+++ com/bridgelabz/util/SpringSeason.java
@@ -6,25 +6,21 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
 public class SpringSeason{
+static int month,year;
   
-/*Main Declaration*/
    public static void main(String args[]) {
- 
-/*Passing arguments to take input*/
-   int month=Integer.parseInt(args[0]);
-   int day=Integer.parseInt(args[1]);
+
+      month=Integer.parseInt(args[0]);
+      day=Integer.parseInt(args[1]);
 
       boolean isSpring=(month==3 && day>=20 && day<=31)
                     || (month==4 && day>=1 && day<=30)
                     || (month==5 && day>=1 && day<=31)
                     || (month==6 && day>=1 && day<=20);
 
-/*prints output*/
-    System.out.println(isSpring);
+      System.out.println(isSpring);
  }
-  }
+}
