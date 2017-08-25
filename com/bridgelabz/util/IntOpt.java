9c991f0a73e2e971adc6c11a5a34e309b33245de
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:26:21 +0530

    Update IntOpt.java

--- com/bridgelabz/util/IntOpt.java
+++ com/bridgelabz/util/IntOpt.java
@@ -6,42 +6,34 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
 class IntOpt {
+	static int input1;
+	static int input2;
+	static int input3;
 
-/*Declaration of Main*/
      public static void main(String[] args) {
 
-/*parse is a method which takes a string(input) as an argument and convert in double.
- input1,input2,input3 are variables which stores the number enterd by the user as input*/ 
+	   input1=Integer.parseInt(args[0]);
+        input2=Integer.parseInt(args[1]);
+        input3=Integer.parseInt(args[2]);
 
-    int input1=Integer.parseInt(args[0]);
-    int input2=Integer.parseInt(args[1]);
-    int input3=Integer.parseInt(args[2]);
-
-
-  /*performs the following arithmatic operstions*/  
         double result1 = (input1 + input2 * input3);
 
-         double result2 = (input1 * input2 + input3);
+        double result2 = (input1 * input2 + input3);
 
-          double result3 = (input1 % input2 + input3); 
+        double result3 = (input1 % input2 + input3); 
 
-/*prints the result*/
 
-    System.out.println("result of" +input1+ "+" +input2+ "*" +input3+ "=" +result1);
+        System.out.println("result of" +input1+ "+" +input2+ "*" +input3+ "=" +result1);
 
-     System.out.println("result of" +input1+ "*" +input2+ "+" +input3+ "=" +result2);
+        System.out.println("result of" +input1+ "*" +input2+ "+" +input3+ "=" +result2);
    
-      System.out.println("result of"+input1+"%"+input2+"+"+input3+"="+result3);     
+        System.out.println("result of"+input1+"%"+input2+"+"+input3+"="+result3);     
   
     }
   }
-
-
     
 
 
