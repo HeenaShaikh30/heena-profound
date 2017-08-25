65b5d565511863b53fb28a30f71201e4535bf511
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:46:41 +0530

    Update ReapetedNumber.java

--- com/bridgelabz/util/ReapetedNumber.java
+++ com/bridgelabz/util/ReapetedNumber.java
@@ -6,19 +6,14 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
 public class ReapetedNumber 
 {   
-/*Declaration of main*/
     public static void main(String[] args) 
     {
-/*Array Declaration*/
         int[] arr = {1,2,3,4,5,2,6,5,8};
  
-/*Initialization of loop*/
         for (int i = 0; i < arr.length-1; i++)
         {
             for (int j = i+1; j < arr.length; j++)
@@ -30,4 +25,3 @@ public class ReapetedNumber
             }
         }
     }    
-}
