60e7a67fb04b0c8e91725238580f1758a9cafeab
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:27:43 +0530

    Update LeapYear.java

--- com/bridgelabz/util/LeapYear.java
+++ com/bridgelabz/util/LeapYear.java
@@ -6,33 +6,24 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
 public class LeapYear{
+static int year;
 
-/*Declaration of Main*/
-  public static void main(String args[]){
+	public static void main(String args[]){
 
+  	int year=Integer.parseInt(args[0]);
 
-/*parse is a method which takes a string(input) as an argument and convert in double.
- year  is variable which stores the number enterd by the user as input*/
-  int year=Integer.parseInt(args[0]);
+        boolean isLeapYear;
 
-   boolean isLeapYear;
+    isLeapYear=(year % 4 == 0);
 
-  /*divisible by 4*/
-  isLeapYear=(year % 4 == 0);
+  	isLeapYear=isLeapYear && (year % 4 != 0);
 
-   /*divisible by 4 and not 100*/
-  isLeapYear=isLeapYear && (year % 4 != 0);
+   	isLeapYear =isLeapYear || (year % 400 == 0);
 
-   /*divisible by 4 and not 100 unless divisible by 400*/
-  isLeapYear =isLeapYear || (year % 400 == 0);
-
-  /*prints the result*/
-    System.out.println(isLeapYear);
+        System.out.println(isLeapYear);
       }
 
    }
