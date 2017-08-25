28ef58e40df36fb5adb0f294a755d0baaf78878c
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 15:11:06 +0530

    Update Trig.java

--- com/bridgelabz/util/Trig.java
+++ com/bridgelabz/util/Trig.java
@@ -6,31 +6,25 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
-class Trig{
+  class Trig{
+  static double degree,radian;
 
-/*Declaration of main*/
- public static void main(String args[]){
+  public static void main(String args[]){
 
-/*passing argument as a input*/
-   double degree=Double.parseDouble(args[0]);
-   double radian=Double.parseDouble(args[1]);
+    degree=Double.parseDouble(args[0]);
+    radian=Double.parseDouble(args[1]);
 
    double s=Math.sin(radian);
    System.out.println("sin("+degree+")="+s);
 
-   
    double c=Math.cos(radian);
    System.out.println("cos("+degree+")="+c);
 
-   
    double t=Math.tan(radian);
    System.out.println("tan("+degree+")="+t);
 
-   
    double r=s*s+c*c;
    System.out.println(s*s+"+"+c*c+"="+r);
   }
