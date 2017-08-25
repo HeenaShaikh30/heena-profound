61e17898dfa3075a6bed3331be056c8e693ff40a
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 15:14:23 +0530

    Update WindChill.java

--- com/bridgelabz/util/WindChill.java
+++ com/bridgelabz/util/WindChill.java
@@ -6,29 +6,21 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
+  public class WindChill {
+  static int temp,velocity;
+  static double result;
 
-public class WindChill {
-
-/*Declaration of main*/
     public static void main(String[] args) {
         
-/*passing command line arguments as a input*/
-  int temp=Integer.parseInt(args[0]);
-  int velocity=Integer.parseInt(args[1]);
+ 	 int temp=Integer.parseInt(args[0]);
+  	 int velocity=Integer.parseInt(args[1]);
+     System.out.println("the entered value of temperature is " +temp+ " and velocity is " +velocity);
 
-/*prints the result*/
-    System.out.println("the entered value of temperature is " +temp+ " and velocity is " +velocity);
-
-      double result = 35.74+0.6215*temp+(0.4275*temp-35.75)*Math.pow(velocity,0.16);
-
-   System.out.println("value of wind chill is" +result);
- }
+         result = 35.74+0.6215*temp+(0.4275*temp-35.75)*Math.pow(velocity,0.16);
+         System.out.println("value of wind chill is" +result);
+ }            
 }
 
     
-    
-
