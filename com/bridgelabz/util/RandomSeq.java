1f0204c2f04a9f2040876dd37a7a9176f873782f
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:43:59 +0530

    Update RandomSeq.java

--- com/bridgelabz/util/RandomSeq.java
+++ com/bridgelabz/util/RandomSeq.java
@@ -6,26 +6,17 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*class Declaration*/
 public class RandomSeq {
-
-/*Declaration of main*/
-  public static void main(String args[]){
-
-/*parse is a method which takes a string(input) as an argument and convert in double.
- inputNo variable which stores the number enterd by the user as input*/ 
-
-    int inputNo=Integer.parseInt(args[0]);
-
-      for(int i=0;i<=inputNo;i++)
-       
-        {
-           System.out.println(Math.random());
+	staic int inputNo;
+public static void main(String args[]){
+ 
+	inputNo=Integer.parseInt(args[0]);
+	for(int i=0;i<=inputNo;i++) 
+{
+        System.out.println(Math.random());
              
-}
-
   }
+ }
 }
