d4ce3d5df934aba4d1cc0d1a9754650f020c8bac
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 15:19:39 +0530

    Update primeChecker.java

--- primeChecker.java
+++ primeChecker.java
@@ -10,19 +10,18 @@
 import java.util.Scanner;
 public class primeChecker{
 
-public static void main(String[] args){
+   public static void main(String[] args){
    Scanner scanner=new Scanner(System.in);
-     int InputNumber, number,ctr1,ctr2=1;
-        System.out.println("Enter a number InputNumber:");
+   int InputNumber, number,ctr1,ctr2=1;
+           System.out.println("Enter a number InputNumber:");
            InputNumber=scanner.nextInt();
            number=InputNumber;
-    for(ctr1=1;ctr1<=100;ctr1++){
-        if((InputNumber%number==0)&&(InputNumber%ctr1==0)){
-         ctr2++;}}
-   if(ctr2==2){
-     System.out.println("prime number is:"+InputNumber);}
-  
-   else{
-      System.out.println("number is not a primr:"+InputNumber);}
+              for(ctr1=1;ctr1<=100;ctr1++){
+              if((InputNumber%number==0)&&(InputNumber%ctr1==0)){
+              ctr2++;}}
+              if(ctr2==2){
+              System.out.println("prime number is:"+InputNumber);}
+              else{
+              System.out.println("number is not a primr:"+InputNumber);}
   }
 }
