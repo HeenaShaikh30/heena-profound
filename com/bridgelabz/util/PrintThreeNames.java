b732faee4be162d90540de5a5cd3d273b23b0c37
HeenaShaikh30 <30771174+HeenaShaikh30@users.noreply.github.com>
2017-08-25 14:37:07 +0530

    Update PrintThreeNames.java

--- com/bridgelabz/util/PrintThreeNames.java
+++ com/bridgelabz/util/PrintThreeNames.java
@@ -6,35 +6,29 @@
 *@version:1.0
 *@since:10-8-2017
 ***************************************************************/
-/*package Naming Style*/
 package com.bridgelabz.util;
 
-/*Import Declaration*/
 import java.util.Scanner;
 
-/*Class Declaration*/
 class PrintThreeNames{
 
-/*Declaration of main */
-  public static void main(String args[]){
-  
-/*Takes input from user*/
-   Scanner scan=new Scanner(System.in);
+  	public static void main(String args[]){
 
-       System.out.println("enter three names");
-     
-        String mName1 = scan.next(); 
-         String mName2 = scan.next();
-          String mName3 = scan.next();
-     
-      /*prints the output*/
+   	Scanner scan=new Scanner(System.in);
 
-       System.out.println(" hi " +mName3);
+        System.out.println("enter three names");
+     
+        	  String mName1 = scan.next(); 
+          	String mName2 = scan.next();
+          	String mName3 = scan.next();
+     
+
+       		 System.out.println(" hi " +mName3);
   
-       System.out.println(" hi " +mName2);
+      		 System.out.println(" hi " +mName2);
            
-        System.out.println(" hi " +mName1);
+       		 System.out.println(" hi " +mName1);
   }
-    }
+ }
 
 
